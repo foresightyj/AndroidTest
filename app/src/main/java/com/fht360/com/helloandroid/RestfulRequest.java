8152c865b87e8ae8bcde0b8cb463ffd2d71583fc
Yuan Jian <imyuanjian@gmail.com>
2017-04-26 16:52:40 +0800

    Still not working

--- app/src/main/java/com/fht360/com/helloandroid/RestfulRequest.java
+++ app/src/main/java/com/fht360/com/helloandroid/RestfulRequest.java
@@ -7,9 +7,12 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.Map;
 
 import static com.android.volley.toolbox.HttpHeaderParser.*;
@@ -20,30 +23,20 @@ import static com.android.volley.toolbox.HttpHeaderParser.*;
 
 public class RestfulRequest<T> extends Request<T> {
     private final Gson gson = new Gson();
-    private Class<T> clazz;
     private final Response.Listener<T> listener;
-
-    @SuppressWarnings("unchecked")
-    private void initClazz() {
-        // the usual verbiage you already have in your question
-        this.clazz = (Class<T>) (((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
-    }
 
     public RestfulRequest(String url, Response.Listener<T> listener, Response.ErrorListener errorListener) {
         super(url, errorListener);
         this.listener = listener;
-        initClazz();
     }
 
     @Override
     protected Response<T> parseNetworkResponse(NetworkResponse response) {
+        Type type = new TypeToken<T>() {
+        }.getType();
         try {
-            String json = new String(
-                    response.data,
-                    parseCharset(response.headers));
-            return Response.success(
-                    gson.fromJson(json, clazz),
-                    parseCacheHeaders(response));
+            String json = new String(response.data, parseCharset(response.headers));
+            return Response.success((T) gson.fromJson(json, type), parseCacheHeaders(response));
         } catch (UnsupportedEncodingException e) {
             return Response.error(new ParseError(e));
         } catch (JsonSyntaxException e) {
